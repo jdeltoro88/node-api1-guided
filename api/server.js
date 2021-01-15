a1e966e745807903389fc9ea37f32219839b64f1
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-01-15 00:55:55 -0700

    2

--- api/server.js
+++ api/server.js
@@ -1,4 +1,5 @@
 // write the server here and export it
+const Dog = require('./dog-model.js');
 const express = require('express');
 const server = express();
 
@@ -21,5 +22,26 @@ server.get('/', (req, res) => {
 server.get('./hello', (req,res) => {
     res.send('wassup')
 })
+
+
+server.post('./api/dogs', async (req,res) => {
+    const dog = req.body;
+
+    //try catch block
+
+    try {
+        const newlyCreatedDog =  Dog.create(dog);
+        res.status(200).json(newlyCreatedDog);
+
+    }catch(err) {
+
+        res.status(500).json({ error:err.message });
+
+    }
+})
+
+
+
+
 
 module.exports = server;
