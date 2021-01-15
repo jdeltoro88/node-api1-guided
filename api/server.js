38c00a768c9ebd7251f0b7b813f8d43201a425e7
jon del toro <61124054+jdeltoro88@users.noreply.github.com>
2021-01-15 00:35:27 -0700

    1

--- api/server.js
+++ api/server.js
@@ -1,1 +1,25 @@
 // write the server here and export it
+const express = require('express');
+const server = express();
+
+
+
+
+//HTTP METHOD, PATH
+
+
+
+
+
+//GET/
+
+
+server.get('/', (req, res) => {
+    res.send('heyaaaaaaa');
+})
+
+server.get('./hello', (req,res) => {
+    res.send('wassup')
+})
+
+module.exports = server;
--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // import the server and start it
+const server = require('./api/server.js');
+
+
+const PORT = 5000;
+
+server.listen(PORT, () => {
+    console.log(`\n*** server listening on port ${PORT} ***\n`)
+})
